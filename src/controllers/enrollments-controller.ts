968096eb8e91ce243029dbec01ac412b2f427d5b
Oscar Maia <oscarfgmaia@gmail.com>
2023-01-22 17:01:47 -0300

    fix: send different error for each case

--- src/controllers/enrollments-controller.ts
+++ src/controllers/enrollments-controller.ts
@@ -1,12 +1,9 @@
 import { AuthenticatedRequest } from '@/middlewares';
 import enrollmentsService from '@/services/enrollments-service';
-import axios from 'axios';
-import { Response, Request } from 'express';
+import { Response } from 'express';
 import httpStatus from 'http-status';
-import { ViaCEPAddress, ViaCEPAddressResponse } from '@/protocols';
 export async function getEnrollmentByUser(req: AuthenticatedRequest, res: Response) {
   const { userId } = req;
-
   try {
     const enrollmentWithAddress = await enrollmentsService.getOneWithAddressByUserId(userId);
 
@@ -16,26 +13,28 @@ export async function getEnrollmentByUser(req: AuthenticatedRequest, res: Respon
   }
 }
 
-//AuthenticatedRequest
 export async function postCreateOrUpdateEnrollment(req: AuthenticatedRequest, res: Response) {
   try {
     await enrollmentsService.createOrUpdateEnrollmentWithAddress({
       ...req.body,
       userId: req.userId,
     });
-
     return res.sendStatus(httpStatus.OK);
   } catch (error) {
     return res.sendStatus(httpStatus.BAD_REQUEST);
   }
 }
-//AuthenticatedRequest
+
 export async function getAddressFromCEP(req: AuthenticatedRequest, res: Response) {
   const { cep } = req.query as Record<string, string>;
   try {
-    const result = await enrollmentsService.getAddressFromCEP(cep)
-    res.send(result)
+    const result = await enrollmentsService.getAddressFromCEP(cep);
+    res.send(result);
   } catch (error) {
-    return res.sendStatus(httpStatus.BAD_REQUEST);
+    if (error.name === 'noContent') {
+      return res.sendStatus(httpStatus.NO_CONTENT);
+    } else {
+      return res.sendStatus(httpStatus.BAD_REQUEST);
+    }
   }
 }
